375432bd03be8c2bf4f8d303e5c4857d034fc8c7
BlindEd <yanek.karpin@gmail.com>
2023-12-08 18:44:01 +0300

    rnjnvd

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 const sum = (a, b, c) => { return a * b + c}
 console.log(sum(4, 7, 1));
+=======
+const sum = (a, b) => { return a + b }
+console.log(sum(4, 7));
+>>>>>>> parent of 2addc58 (this commit must die)
